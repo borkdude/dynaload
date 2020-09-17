3f473cf8b59e9e270277b7f82e15a2893ecaa94a
Michiel Borkent <michielborkent@gmail.com>
2020-09-17 11:12:45 +0200

    Add GraalVM test

---
+++ graal-test/src/hello_world/core.clj
@@ -0,0 +1,11 @@
+(ns hello-world.core
+  (:gen-class)
+  (:require [borkdude.dynaload-clj :as dyn]))
+
+(def eval-string* (dyn/dynaload 'sci.core/eval-string {:default (fn [_] :dude)}))
+
+(defn eval-string [s]
+  (@eval-string* s))
+
+(defn -main [& _args]
+  (prn (eval-string "(+ 1 2 3)")))
